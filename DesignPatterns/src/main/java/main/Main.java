445a50c5f1eac8fa09e66c4f2374a6111fc30761
Shirali <shiralialihummatov@gmail.com>
2022-06-27 17:54:11 +0400

    changes

--- DesignPatterns/src/main/java/main/Main.java
+++ DesignPatterns/src/main/java/main/Main.java
@@ -4,23 +4,46 @@ import myPatterns.creationalPatterns.abstractFactoryPattern.FactoryProducer;
 import myPatterns.creationalPatterns.abstractFactoryPattern.Note10;
 import myPatterns.creationalPatterns.abstractFactoryPattern.PhoneAbstractFactory;
 import myPatterns.creationalPatterns.abstractFactoryPattern.SamsungPhone;
+import myPatterns.creationalPatterns.builderPattern.BuilderClass;
+import myPatterns.creationalPatterns.factoryMethodPattern.Phone;
+import myPatterns.creationalPatterns.factoryMethodPattern.PhoneFactory;
 import myPatterns.creationalPatterns.singletonPattern.SingletonClass;
 
 public class Main {
-//    public static void main(String[] args) {
-//        new BuilderClass.Builder(1,5).demir(67).protein(9).build();
-//    }
-//
-//    public static void main(String[] args) {
-//        for (int i = 0; i < 10; i++) {
-//            SingletonClass mySingleton = SingletonClass.getSingleton();
-//        }
-//    }
 
+    // Builder pattern
+    /*
     public static void main(String[] args) {
-        PhoneAbstractFactory noteFactory =  FactoryProducer.getFactory("note");
+        new BuilderClass.Builder(1,5).demir(67).protein(9).build();
+    }
+    */
+
+    // Singleton pattern
+    /*
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            SingletonClass mySingleton = SingletonClass.getSingleton();
+        }
+    }
+    */
+
+    // Factory method pattern
+    /*
+    public static void main(String[] args) {
+        Phone s8 = PhoneFactory.getPhone("s8", "3000", 3, 6);
+        System.out.println(s8);
+    }
+    */
+
+
+    // Abstract Factory pattern
+    /*
+    public static void main(String[] args) {
+        PhoneAbstractFactory noteFactory = FactoryProducer.getFactory("note");
         SamsungPhone note10 = noteFactory.getPhone("note10", "2000", 4, 10);
         System.out.println(note10);
     }
+    */
+
 
 }
